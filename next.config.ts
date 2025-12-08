88197d9d6f9730bf9d7ebdf0a2b86f8c66f2e2a2
Yu Yin <136771156+Nimbus318@users.noreply.github.com>
2025-12-08 12:14:43 +0800

    chore: Configure Cloudflare Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'mmbiz.qpic.cn',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
